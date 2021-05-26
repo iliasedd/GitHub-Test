d24782a204e5f2fe8abedc10fee885573def33c6
iliasedd <iliasman2400@gmail.com>
2021-05-26 14:52:22 +0100

    add js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let myHeader = document.getElementById("header-id")
+    setTimeout(() => {
+        myHeader.classList.remove("hidden")
+    }, 3000);
+})
